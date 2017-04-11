f6a9cdda86a895fa6df4cd4154ec2c713c36a9a1
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-11 17:01:48 +0700

    Add Alternative abstract model

---
+++ src/main/java/factory/Alternative.java
@@ -0,0 +1,20 @@
+package factory;
+
+import factory.interfaces.AlternativeComparator;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This <wp> created by : 
+ * Name         : syafiq
+ * Date / Time  : 11 April 2017, 4:01 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class Alternative<WC extends WeightContainer, PA extends Accumulator> implements AlternativeComparator<Alternative>
+{
+    public abstract void calculatePreferences(@NotNull WC alternative);
+
+    public abstract void collectPreferences(@NotNull PA accumulator);
+
+    public abstract void ranking(@NotNull PA accumulator);
+}
