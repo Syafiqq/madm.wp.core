5052722a2917eb1f5bdd6103386fb0761eb83739
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-11 17:01:16 +0700

    Add Properties abstract model

---
+++ src/main/java/factory/Properties.java
@@ -0,0 +1,12 @@
+package factory;
+
+/*
+ * This <wp> created by : 
+ * Name         : syafiq
+ * Date / Time  : 11 April 2017, 4:03 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class Properties
+{
+}
