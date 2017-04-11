9c55cbc6094154862082b43c383e065554e61be6
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-11 17:19:23 +0700

    Add WeightContainer instanced class

---
+++ case/src/main/java/case_0/weight/ContinuousWeight.java
@@ -0,0 +1,57 @@
+package case_0.weight;
+
+import case_0.accumulator.DoubleAccumulator;
+import factory.Weight;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.jetbrains.annotations.NotNull;
+
+/*
+ * This <wp> created by : 
+ * Name         : syafiq
+ * Date / Time  : 11 April 2017, 4:15 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class ContinuousWeight extends Weight<DoubleAccumulator>
+{
+    private double weight;
+    private double normalized;
+
+    public ContinuousWeight(double weight)
+    {
+        this.weight = weight;
+    }
+
+    public double getNormalized()
+    {
+        return this.normalized;
+    }
+
+    public void setNormalized(double normalized)
+    {
+        this.normalized = normalized;
+    }
+
+    public double getWeight()
+    {
+        return this.weight;
+    }
+
+    public void setWeight(double weight)
+    {
+        this.weight = weight;
+    }
+
+    @Override public void normalize(@NotNull DoubleAccumulator accumulator)
+    {
+        this.normalized = this.weight / accumulator.getValue();
+    }
+
+    @Override public String toString()
+    {
+        return new ToStringBuilder(this)
+                .append("weight", weight)
+                .append("normalized", normalized)
+                .toString();
+    }
+}
