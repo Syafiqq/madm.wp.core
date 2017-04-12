a01b081fd4a3a792fb635a042853d8a41962488b
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-12 07:10:34 +0700

    Add case 1: criterion type

---
+++ case/src/main/java/case_1/type/CriterionType.java
@@ -0,0 +1,14 @@
+package case_1.type;
+
+/*
+ * This <wp> created by : 
+ * Name         : syafiq
+ * Date / Time  : 11 April 2017, 5:29 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public enum CriterionType
+{
+    BENEFIT,
+    COST,
+}
